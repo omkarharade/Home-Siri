361cb0e69f64ced88621cfa511dd3f5166308cb4
Omkar Harade <omkarharade1301@gmail.com>
2025-02-12 01:45:33 +0530

    filter visible on home.ejs if premium applied

--- Home/public/js/filter.js
+++ Home/public/js/filter.js
@@ -1,13 +1,8 @@
 // Check membership status and show/hide filter
 document.addEventListener('DOMContentLoaded', () => {
-    const membershipStatus = localStorage.getItem('membershipStatus');
-    const filterContainer = document.getElementById('filter-container');
 
-    if (membershipStatus === 'premium') {
-        filterContainer.style.display = 'block';
-    }
+    loadFilterIfPremium();
 
-    // Handle filter selection
     const filterSelect = document.getElementById('filter-select');
     filterSelect.addEventListener('change', () => {
         const selectedValue = filterSelect.value;
@@ -16,3 +11,54 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+async function isPremium() {
+
+    try {
+        const userId = await JSON.parse(
+            document.getElementById("user-id").getAttribute("data-userId")
+        );
+
+        const params = new URLSearchParams({
+            user_id: userId,
+        });
+
+        const url = `http://localhost:5000/api/auth/premium?${params}`;
+
+        console.log("inside isPremium and userId = ", userId);
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+
+
+        const result = await response.json();
+
+        if (response.ok) {
+            console.log("user status is ", result.havePremium);
+        } else {
+            alert("Something went wrong: " + result.message);
+        }
+
+        return result.havePremium
+
+    } catch (error) {
+        alert("internal server error");
+    }
+
+}
+
+async function loadFilterIfPremium(){
+
+    const membershipStatus = await isPremium();
+    console.log("membership status and inside filter == ", membershipStatus);
+    const filterContainer = document.getElementById('filter-container');
+
+    if (membershipStatus) {
+        filterContainer.style.display = 'block';
+    }
+
+    // Handle filter selection
+}
--- Home/public/js/profile.js
+++ Home/public/js/profile.js
@@ -13,3 +13,46 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "/profile";
     });
 });
+
+
+async function isPremium() {
+
+    try {
+        const userId = await JSON.parse(
+            document.getElementById("user-id").getAttribute("data-userId")
+        );
+
+        const params = new URLSearchParams({
+            user_id: userId,
+        });
+
+        const url = `http://localhost:5000/api/auth/premium?${params}`;
+
+        console.log("inside isPremium and userId = ", userId);
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+
+
+        const result = await response.json();
+
+        if (response.ok) {
+            console.log("user status is ", result.havePremium);
+        } else {
+            alert("Something went wrong: " + result.message);
+        }
+
+        return result.havePremium
+
+    } catch (error) {
+        alert("internal server error");
+    }
+
+}
+async function isPremiumFunc() {
+
+    return await isPremium();
+}
+
